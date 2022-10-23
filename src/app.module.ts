073e41d6ea77bf72908c96d0497550cbf084d640
omar fareed <fareedomar159@gmail.com>
2022-10-23 22:15:41 +0200

    connected to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    // for using .env variables
+    ConfigModule.forRoot(),
+    // connect to database using connection string
+    MongooseModule.forRoot(process.env.DB_CONNECTION_STRING),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
