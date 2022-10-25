188a7ee7e869fae20c0598d5b990b385dbc4a483
Mohamed Kamal <medoking91@gmail.com>
2022-10-25 11:47:51 +0200

    docs_24_create-swagger-openapi
    - Added user and comment resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
+import { PostModule } from './post/post.module';
+import { CommentModule } from './comment/comment.module';
 @Module({
   imports: [
     // for using .env variables
     ConfigModule.forRoot(),
     // connect to database using connection string
     MongooseModule.forRoot(process.env.DB_CONNECTION_STRING),
+    PostModule,
+    CommentModule,
   ],
   controllers: [],
   providers: [],
