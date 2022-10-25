0a6668ea97ef61333a1b1bb9431db2ed57200567
Mohamed Kamal <medoking91@gmail.com>
2022-10-25 14:22:34 +0200

    docs_24_create-swagger-openapi
    - created base docs for user and comments

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -1,34 +1,155 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import {
+  ApiCreatedResponse,
+  ApiForbiddenResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
 import { CommentService } from './comment.service';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { UpdateCommentDto } from './dto/update-comment.dto';
 
+@ApiTags('comment')
 @Controller('comment')
 export class CommentController {
   constructor(private readonly commentService: CommentService) {}
 
+  @ApiOperation({ description: 'Submit a new comment.' })
+  @ApiCreatedResponse({ description: 'The resource was created successfully' })
+  @ApiForbiddenResponse({ description: 'Unauthorized Request' })
+  // @ApiBody({ description: 'Create some resource' })
   @Post()
   create(@Body() createCommentDto: CreateCommentDto) {
     return this.commentService.create(createCommentDto);
   }
 
-  @Get()
-  findAll() {
-    return this.commentService.findAll();
-  }
+  // @Get()
+  // findAll() {
+  //   return this.commentService.findAll();
+  // }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.commentService.findOne(+id);
-  }
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.commentService.findOne(+id);
+  // }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCommentDto: UpdateCommentDto) {
-    return this.commentService.update(+id, updateCommentDto);
-  }
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateCommentDto: UpdateCommentDto) {
+  //   return this.commentService.update(+id, updateCommentDto);
+  // }
 
+  @ApiOperation({ description: 'Deletes a comment.' })
+  @ApiOkResponse({ description: 'The resource was deleted successfully' })
+  @ApiForbiddenResponse({ description: 'Unauthorized Request' })
+  @ApiNotFoundResponse({ description: 'Resource not found' })
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.commentService.remove(+id);
+  }
+
+  @ApiOperation({ description: 'Edit the body text of a comment.' })
+  @ApiOkResponse({ description: 'The resource was updated successfully' })
+  @ApiNotFoundResponse({ description: 'Resource not found' })
+  @ApiForbiddenResponse({ description: 'Unauthorized Request' })
+  @Patch('/edit')
+  //todo
+  edit(@Body() body: any) {
+    return body;
+  }
+
+  @ApiOperation({
+    description: 'Prevents new child comments from receiving new comments.',
+  })
+  @ApiOkResponse({ description: 'Successful comment lock' })
+  @ApiNotFoundResponse({ description: 'Resource not found' })
+  @ApiForbiddenResponse({ description: 'Unauthorized Request' })
+  @Post('/lock')
+  //todo
+  lock(@Body() body: any) {
+    return body;
+  }
+
+  @ApiOperation({
+    description: 'Unlock a comment, allows a comment to receive new comments.',
+  })
+  @ApiOkResponse({ description: 'Successful comment unlock' })
+  @ApiNotFoundResponse({ description: 'Resource not found' })
+  @ApiForbiddenResponse({ description: 'Unauthorized Request' })
+  @Post('/unlock')
+  //todo
+  unlock(@Body() body: any) {
+    return body;
+  }
+
+  @ApiOperation({
+    description:
+      "Brings it to the attention of the subreddit's moderators and marks it as spam.",
+  })
+  @ApiOkResponse({ description: 'Successful comment report spam' })
+  @ApiNotFoundResponse({ description: 'Resource not found' })
+  @ApiForbiddenResponse({ description: 'Unauthorized Request' })
+  @Post('/spam')
+  //todo
+  spam(@Body() body: any) {
+    return body;
+  }
+
+  @ApiOperation({
+    description:
+      "Save comment, Saved things are kept in the user's saved listing for later perusal.",
+  })
+  @ApiCreatedResponse({ description: 'Successful comment save' })
+  @ApiNotFoundResponse({ description: 'Resource not found' })
+  @ApiForbiddenResponse({ description: 'Unauthorized Request' })
+  @Post('/save')
+  //todo
+  save(@Body() body: any) {
+    return body;
+  }
+  @ApiOperation({
+    description:
+      "UnSave a link or comment, this removes the thing from the user's saved listings as well.",
+  })
+  @ApiCreatedResponse({ description: 'Successful comment unsave' })
+  @ApiNotFoundResponse({ description: 'Resource not found' })
+  @ApiForbiddenResponse({ description: 'Unauthorized Request' })
+  @Post('/unsave')
+  //todo
+  unSave(@Body() body: any) {
+    return body;
+  }
+
+  @ApiOperation({
+    description: 'Enable or disable inbox replies for a comment.',
+  })
+  @ApiOkResponse({ description: 'Successful comment set inbox replies' })
+  @ApiNotFoundResponse({ description: 'Resource not found' })
+  @ApiForbiddenResponse({ description: 'Unauthorized Request' })
+  @Post('/send_replies')
+  //todo
+  sendReplies(@Body() body: any) {
+    return body;
+  }
+
+  @ApiOperation({
+    description: 'Cast a vote on a comment.',
+  })
+  @ApiOkResponse({ description: 'Successful comment vote' })
+  @ApiNotFoundResponse({ description: 'Resource not found' })
+  @ApiForbiddenResponse({ description: 'Unauthorized Request' })
+  @Post('/vote')
+  //todo
+  vote(@Body() body: any) {
+    return body;
   }
 }
--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,34 +1,235 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { PostService } from './post.service';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
+import {
+  ApiCreatedResponse,
+  ApiForbiddenResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+  ApiUnprocessableEntityResponse,
+} from '@nestjs/swagger';
 
+@ApiTags('Post')
 @Controller('post')
 export class PostController {
   constructor(private readonly postService: PostService) {}
 
-  @Post()
+  @ApiOperation({ description: 'Submit a post to a subreddit.' })
+  @ApiCreatedResponse({ description: 'The resource was created successfully' })
+  @ApiForbiddenResponse({ description: 'Unauthorized Request' })
+  @ApiUnprocessableEntityResponse({
+    description:
+      'If a link with the same URL has already been submitted to the specified subreddit an error will be returned unless resubmit is true.',
+  })
+  @Post('/submit')
   create(@Body() createPostDto: CreatePostDto) {
     return this.postService.create(createPostDto);
   }
 
-  @Get()
-  findAll() {
-    return this.postService.findAll();
-  }
+  // @Get()
+  // findAll() {
+  //   return this.postService.findAll();
+  // }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.postService.findOne(+id);
-  }
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.postService.findOne(+id);
+  // }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updatePostDto: UpdatePostDto) {
-    return this.postService.update(+id, updatePostDto);
-  }
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updatePostDto: UpdatePostDto) {
+  //   return this.postService.update(+id, updatePostDto);
+  // }
 
+  @ApiOperation({ description: 'Deletes a post.' })
+  @ApiOkResponse({ description: 'The resource was deleted successfully' })
+  @ApiForbiddenResponse({ description: 'Unauthorized Request' })
+  @ApiNotFoundResponse({ description: 'Resource not found' })
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.postService.remove(+id);
+  }
+  @ApiOperation({ description: 'Edit the body text of a post.' })
+  @ApiOkResponse({ description: 'The resource was updated successfully' })
+  @ApiNotFoundResponse({ description: 'Resource not found' })
+  @ApiForbiddenResponse({ description: 'Unauthorized Request' })
+  @Patch('/edit')
+  //todo
+  edit(@Body() body: any) {
+    return body;
+  }
+  @ApiOperation({
+    description: `Follow or unFollow a post.    
+       To follow, follow should be True.
+       To unFollow, follow should be False. 
+       The user must have access to the subreddit to be able to follow a post within it.`,
+  })
+  @ApiOkResponse({ description: 'The resource was updated successfully' })
+  @ApiNotFoundResponse({ description: 'Resource not found' })
+  @ApiForbiddenResponse({ description: 'Unauthorized Request' })
+  @Post('/follow')
+  //todo
+  follow(@Body() body: any) {
+    return body;
+  }
+  @ApiOperation({
+    description: `Hide a post, this removes it from the user's default view of subreddit listings.`,
+  })
+  @ApiOkResponse({ description: `Successful post hide` })
+  @ApiNotFoundResponse({ description: 'Resource not found' })
+  @ApiForbiddenResponse({ description: 'Unauthorized Request' })
+  @Post('/hide')
+  //todo
+  hide(@Body() body: any) {
+    return body;
+  }
+  @ApiOperation({
+    description: `UnHide a post, this removes the hide property from a post thus it can reappear again.`,
+  })
+  @ApiOkResponse({ description: `Successful post unhide` })
+  @ApiNotFoundResponse({ description: 'Resource not found' })
+  @ApiForbiddenResponse({ description: 'Unauthorized Request' })
+  @Post('/unhide')
+  //todo
+  unhide(@Body() body: any) {
+    return body;
+  }
+  @ApiOperation({
+    description: `Lock a post. Prevents a post from receiving new comments.`,
+  })
+  @ApiOkResponse({ description: `Successful post lock` })
+  @ApiNotFoundResponse({ description: 'Resource not found' })
+  @ApiForbiddenResponse({ description: 'Unauthorized Request' })
+  @Post('/lock')
+  //todo
+  lock(@Body() body: any) {
+    return body;
+  }
+  @ApiOperation({
+    description: `UnLock a post. Prevents a post from receiving new comments.`,
+  })
+  @ApiOkResponse({ description: `Successful post Unlock` })
+  @ApiNotFoundResponse({ description: 'Resource not found' })
+  @ApiForbiddenResponse({ description: 'Unauthorized Request' })
+  @Post('/unlock')
+  //todo
+  unlock(@Body() body: any) {
+    return body;
+  }
+  @ApiOperation({ description: `UnMark a post NSFW.` })
+  @ApiOkResponse({ description: `Successful post mark` })
+  @ApiNotFoundResponse({ description: 'Resource not found' })
+  @ApiForbiddenResponse({ description: 'Unauthorized Request' })
+  @Post('/mark_nsfw')
+  //todo
+  markNsfw(@Body() body: any) {
+    return body;
+  }
+  @ApiOperation({ description: `Mark a post NSFW.` })
+  @ApiOkResponse({ description: `Successful post mark` })
+  @ApiNotFoundResponse({ description: 'Resource not found' })
+  @ApiForbiddenResponse({ description: 'Unauthorized Request' })
+  @Post('/unmark_nsfw')
+  //todo
+  unMarkNsfw(@Body() body: any) {
+    return body;
+  }
+  @ApiOperation({
+    description: `Brings it to the attention of the subreddit's moderators and marks it as spam.`,
+  })
+  @ApiOkResponse({ description: `Successful post report spam` })
+  @ApiNotFoundResponse({ description: 'Resource not found' })
+  @ApiForbiddenResponse({ description: 'Unauthorized Request' })
+  @Post('/spam')
+  //todo
+  spam(@Body() body: any) {
+    return body;
+  }
+  @ApiOperation({ description: `Enable or disable inbox replies for a post.` })
+  @ApiOkResponse({ description: `Successful post replies set` })
+  @ApiNotFoundResponse({ description: 'Resource not found' })
+  @ApiForbiddenResponse({ description: 'Unauthorized Request' })
+  @Post('/send_replies')
+  //todo
+  sendReplies(@Body() body: any) {
+    return body;
+  }
+  @ApiOperation({
+    description: `Flag the post as spoiler.`,
+  })
+  @ApiOkResponse({ description: `Successful post spoiler` })
+  @ApiNotFoundResponse({ description: 'Resource not found' })
+  @ApiForbiddenResponse({ description: 'Unauthorized Request' })
+  @Post('/spoiler')
+  //todo
+  spoiler(@Body() body: any) {
+    return body;
+  }
+  @ApiOperation({
+    description: `Flag the post as not spoiler.`,
+  })
+  @ApiOkResponse({ description: `Successful post unSpoiler` })
+  @ApiNotFoundResponse({ description: 'Resource not found' })
+  @ApiForbiddenResponse({ description: 'Unauthorized Request' })
+  @Post('/unspoiler')
+  //todo
+  unSpoiler(@Body() body: any) {
+    return body;
+  }
+  @ApiOperation({
+    description: `Cast a vote on a post.`,
+  })
+  @ApiOkResponse({ description: `Successful post vote` })
+  @ApiNotFoundResponse({ description: 'Resource not found' })
+  @ApiForbiddenResponse({ description: 'Unauthorized Request' })
+  @Post('/vote')
+  //todo
+  vote(@Body() body: any) {
+    return body;
+  }
+  @ApiOperation({
+    description: `Save post, Saved things are kept in the user's saved listing for later perusal.`,
+  })
+  @ApiOkResponse({ description: `Successful post save` })
+  @ApiNotFoundResponse({ description: 'Resource not found' })
+  @ApiForbiddenResponse({ description: 'Unauthorized Request' })
+  @Post('/save')
+  //todo
+  save(@Body() body: any) {
+    return body;
+  }
+  @ApiOperation({
+    description: `UnSave post, Saved things are kept in the user's saved listing for later perusal.`,
+  })
+  @ApiOkResponse({ description: `Successful post unsave` })
+  @ApiNotFoundResponse({ description: 'Resource not found' })
+  @ApiForbiddenResponse({ description: 'Unauthorized Request' })
+  @Post('/unsave')
+  //todo
+  unSave(@Body() body: any) {
+    return body;
+  }
+  @ApiOperation({
+    description: `Set a suggested sort for a link.
+     Suggested sorts are useful to display comments in a certain preferred way for posts.`,
+  })
+  @ApiOkResponse({ description: `Successful post suggested sort set` })
+  @ApiNotFoundResponse({ description: 'Resource not found' })
+  @ApiForbiddenResponse({ description: 'Unauthorized Request' })
+  @Post('/set_suggested_sort')
+  //todo
+  setSuggestedSort(@Body() body: any) {
+    return body;
   }
 }
