b500a54b443f33a6748d37f1f6722044dac801a9
Mohamed Kamal <medoking91@gmail.com>
2022-10-28 12:58:39 +0200

    22-email-settings-requests
    - Added docs

--- src/utils/mail.service.ts
+++ src/utils/mail.service.ts
@@ -3,8 +3,19 @@ import { Global, HttpStatus, Injectable } from '@nestjs/common';
 
 @Global()
 @Injectable()
+/**
+ * A utility class to send emails.
+ */
 export class EmailService {
   constructor(private mailService: MailerService) {}
+  /**
+   * A utility function to send emails with variable user email, subject, and body.
+   *
+   * @param toMail The user email that will be sent to.
+   * @param subject The email subject.
+   * @param body The email body.
+   * @returns Status code OK if sent successfully otherwise UNAUTHORIZED
+   */
   async sendEmail(toMail: string, subject: string, body: string) {
     try {
       const response = await this.mailService.sendMail({
