63e434c8855f71d4da04cb636961edd85ba1e755
A7medAref <ahmedmostafa992001@gmail.com>
2022-11-03 19:11:07 +0200

    Modifing the main to match the development branch

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,10 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    rawBody: true,
-  });
+  const app = await NestFactory.create(AppModule);
 
   // Prefix all endpoinds with api/
   app.setGlobalPrefix('api');
@@ -31,6 +30,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   await app.listen(3000);
 }
 bootstrap();
