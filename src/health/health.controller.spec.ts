52aafbda7d0c4ab4e37c8ee08479257f65005ea2
Mohammed Abdulhady <m.abdulhady00@gmail.com>
2022-11-10 22:55:40 +0200

    test: create spec for check method in health controller

--- src/health/health.controller.spec.ts
+++ src/health/health.controller.spec.ts
@@ -1,15 +1,22 @@
+import type { HealthCheckResult } from '@nestjs/terminus';
 import type { TestingModule } from '@nestjs/testing';
 import { Test } from '@nestjs/testing';
+import mongoose from 'mongoose';
 
+import {
+  closeInMongodConnection,
+  rootMongooseTestModule,
+} from '../utils/mongoose-in-memory';
 import { HealthController } from './health.controller';
 import { HealthModule } from './health.module';
+import { healthCheckStub } from './test/stubs/health.stubs';
 
 describe('HealthController', () => {
   let controller: HealthController;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [HealthModule],
+      imports: [rootMongooseTestModule(), HealthModule],
     }).compile();
 
     controller = module.get<HealthController>(HealthController);
@@ -17,5 +24,17 @@ describe('HealthController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should return a proper health check result', async () => {
+    const healthCheckResult: HealthCheckResult = await controller.check();
+    expect(healthCheckResult).toEqual(
+      expect.objectContaining(healthCheckStub()),
+    );
+  });
+
+  afterAll(async () => {
+    await closeInMongodConnection();
+    await mongoose.disconnect();
   });
 });
