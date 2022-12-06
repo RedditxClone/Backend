ebd390ee097905d9f9750ff31c38246692978377
A7medAref <ahmedmostafa992001@gmail.com>
2022-12-06 10:34:47 +0200

    solving merge issue

--- src/search/search.service.ts
+++ src/search/search.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
+import type { Comment } from '../comment/comment.schema';
 import type { Post } from '../post/post.schema';
-import type { PostComment } from '../post-comment/post-comment.schema';
 import type { Subreddit } from '../subreddit/subreddit.schema';
 // import { BlockService } from '../block/block.service';
 import type { User } from '../user/user.schema';
@@ -15,7 +15,7 @@ export class SearchService {
     @InjectModel('User') private readonly userModel: Model<User>,
     @InjectModel('post') private readonly postModel: Model<Post>,
     @InjectModel('Subreddit') private readonly subredditModel: Model<Subreddit>,
-    private readonly postCommentModel: Model<PostComment>,
+    @InjectModel('Comment') private readonly commentModel: Model<Comment>,
     private readonly apiFeaturesService: ApiFeaturesService, // private readonly blockService: BlockService,
   ) {}
 
@@ -32,6 +32,16 @@ export class SearchService {
       { pagination: true },
     );
 
+  searchCommunities = (data: string, numberOfData: number) =>
+    this.apiFeaturesService.processQuery(
+      this.subredditModel.find(
+        { name: new RegExp(`^${data}`) },
+        { description: new RegExp(`*${data}*`) },
+      ),
+      { limit: numberOfData },
+      { pagination: true },
+    );
+
   searchPosts = async (data: string, numberOfData: number) =>
     this.apiFeaturesService.processQuery(
       this.postModel.find({
@@ -44,19 +54,24 @@ export class SearchService {
       { pagination: true },
     );
 
-  searchCommunities = (data: string, numberOfData: number) =>
-    this.apiFeaturesService.processQuery(
-      this.subredditModel.find(
-        { name: new RegExp(`^${data}`) },
-        { description: new RegExp(`*${data}*`) },
-      ),
-      { limit: numberOfData },
-      { pagination: true },
-    );
-
   searchComments = (data: string, numberOfData: number) =>
     this.apiFeaturesService.processQuery(
-      this.postCommentModel.find({ type: new RegExp(`*${data}*`) }),
+      this.commentModel.find({ text: new RegExp(`*${data}*`) }).populate([
+        {
+          path: 'postId',
+          model: 'Post',
+          select: 'title publishedDate',
+          // populate: {
+          //   path: 'country',
+          //   model: 'Country',
+          // },
+        },
+        {
+          path: 'userId',
+          model: 'User',
+          select: 'username profilePhoto',
+        },
+      ]),
       { limit: numberOfData },
       { pagination: true },
     );
