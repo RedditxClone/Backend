4488db215b24c4d36c48b32c7356593d01a2a4f7
A7medAref <ahmedmostafa992001@gmail.com>
2022-12-07 13:12:19 +0200

    adding general search

--- src/search/search.controller.ts
+++ src/search/search.controller.ts
@@ -37,24 +37,34 @@ export class SearchController {
     return this.searchService.searchPeople(word, page, 10, req._id);
   }
 
-  @ApiProperty({ description: 'search for people' })
+  @ApiProperty({ description: 'search for communities' })
   @Get('/communities')
   searchCommunities(@Query('word') word, @Query('page') page) {
     return this.searchService.searchCommunities(word, page, 10);
   }
 
   @UseGuards(IsUserExistGuard)
-  @ApiProperty({ description: 'search for people' })
+  @ApiProperty({ description: 'search for posts' })
   @Get('/posts')
   searchPosts(@Query('word') word, @Query('page') page, @Req() req) {
     return this.searchService.searchPosts(word, page, 10, req._id);
   }
 
   @UseGuards(IsUserExistGuard)
-  @ApiProperty({ description: 'search for people' })
+  @ApiProperty({ description: 'search for comments' })
   @Get('/comments')
   searchComments(@Query('word') word, @Query('page') page, @Req() req) {
     return this.searchService.searchComments(word, page, 10, req._id);
+  }
+
+  @UseGuards(IsUserExistGuard)
+  @ApiProperty({ description: 'genral search' })
+  @Get('/general')
+  searchGeneral(@Query('word') word, @Req() req) {
+    return Promise.all([
+      this.searchService.searchPeople(word, 1, 5, req._id),
+      this.searchService.searchCommunities(word, 1, 5),
+    ]);
   }
 
   @ApiProperty({ description: 'get subreddit search results' })
