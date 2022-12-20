87d8adddbb96dce635f95563bdb0f272573d03c8
Mohamed Kamal <medoking91@gmail.com>
2022-12-20 17:19:09 +0200

    fix: lint violations

--- src/subreddit/subreddit.service.ts
+++ src/subreddit/subreddit.service.ts
@@ -510,7 +510,6 @@ export class SubredditService {
     type: string | undefined,
   ) {
     await this.checkIfModerator(srName, modUsername);
-    console.log(srName);
 
     return this.postCommentService.getUnModeratedThingsForSubreddit(
       srName,
